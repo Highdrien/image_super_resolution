0ec4209c009bb430cc602bc7572e7e115a94c198
Highdrien <adrien.zabban@outlook.fr>
2023-05-23 19:41:55 +0200

    add test.py and evaluate the experiment 0

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from easydict import EasyDict as edict
 
 from src.dataloader import getbatch
 from src.train import train
+from src.test import test
 
 
 def load_config(path='configs/config.yaml'):
@@ -21,6 +22,10 @@ def main(options):
         config = load_config(options['config_path'])
         train(config)
     
+    elif options['mode'] == 'test':
+        config = load_config(options['config_path'])
+        test(options['path'], config)
+
     else:
         print(options['mode'])
         print('ERROR')
@@ -33,6 +38,7 @@ if __name__ == "__main__":
     # Options
     parser.add_argument('--mode', default=None, type=str, help="choose a mode between 'data', 'train', 'test' and 'predict'")
     parser.add_argument('--config_path', default=os.path.join('config', 'config.yaml'), type=str, help="path to config")
+    parser.add_argument('--path', type=str, help="experiment path")
 
     args = parser.parse_args()
     options = vars(args)
