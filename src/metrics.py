701ea9c1d1d0d91538760e90105e3568d578a1e5
Peyremorte Romain <romain.peyremorte@yahoo.fr>
2023-05-25 12:16:16 +0200

    ajout ms_ssim

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 from torchmetrics import PeakSignalNoiseRatio
-
+from torchmetrics import MultiScaleStructuralSimilarityIndexMeasure
 
 def compute_metrics(config, y_pred, y_true):
     metrics = []
@@ -7,5 +7,12 @@ def compute_metrics(config, y_pred, y_true):
     if config.metrics.PSNR:
         psnr = PeakSignalNoiseRatio()
         metrics.append(psnr(y_pred, y_true))
-    
+    # PSRN calculate difference pixel per pixel
+
+    if config.metrics.PSNR:
+        ms_ssim = MultiScaleStructuralSimilarityIndexMeasure(data_range=1.0)
+        metrics.append(ms_ssim(y_pred, y_true))
+    # MS-SSIM is a multi-scale metrics based on SSIM
+    # SSIM is more a visual metrics compared to PSNR: it looks at the luminance, contrast and structure to find visible differences
+    # MS-SSIM is application of SSIM performed at multiple scales through a multi-step downsampling process to have better result.
     return metrics
