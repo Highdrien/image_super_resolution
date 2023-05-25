988606ef1cabe17fd711985ea52bc1794d0bd7e1
Highdrien <adrien.zabban@outlook.fr>
2023-05-25 19:37:07 +0200

    add MSSSIM metrics on the bicubic

--- src/bicubic.py
+++ src/bicubic.py
@@ -21,41 +21,47 @@ def bicubic(lr_image, up_scale_factor):
 
 
 def test_bicubic(config):
-    test_generator = create_generator(config, mode='test')
-    logging_path = os.path.join("logs", "bicubic")
 
-    # Loss
-    if config.model.loss.lower() == 'mse':
-        criterion = torch.nn.MSELoss()
-    else:
-        raise 'MSE loss is the only one to be implemented'
-    
-    # Metrics
-    metrics_name = list(filter(lambda x: config.metrics[x], config.metrics))
+    for i in range(2, 5):
+        config.upscale_factor = i
+        print("upscale factor:", i)
 
-    ###############################################################
-    # Start Evaluation                                            #
-    ###############################################################
+        test_generator = create_generator(config, mode='test')
+        logging_path = os.path.join("logs", "bicubic")
 
-    test_loss = 0
-    test_metrics = np.zeros(len(metrics_name), dtype=float)
-
-    for (lr_image, y_true) in tqdm(test_generator):
-
-        y_pred = bicubic(lr_image, config.upscale_factor)
-
-        loss = criterion(y_pred, y_true)
-        test_loss += loss.item()
-        test_metrics += compute_metrics(config, y_pred.detach(), y_true.detach())
+        # Loss
+        if config.model.loss.lower() == 'mse':
+            criterion = torch.nn.MSELoss()
+        else:
+            raise 'MSE loss is the only one to be implemented'
         
-    test_loss = test_loss / len(test_generator)
-    print('test loss:', test_loss)
+        # Metrics
+        metrics_name = list(filter(lambda x: config.metrics[x], config.metrics))
 
-    if not(os.path.exists(logging_path)):
-        os.mkdir(logging_path)
+        ###############################################################
+        # Start Evaluation                                            #
+        ###############################################################
 
-    metrics_name = [config.model.loss] + metrics_name
-    metrics_value = [test_loss] + list(test_metrics)
+        test_loss = 0
+        test_metrics = np.zeros(len(metrics_name), dtype=float)
 
-    test_logger(logging_path, metrics_name, metrics_value, config.upscale_factor)
+        for (lr_image, y_true) in tqdm(test_generator):
+
+            y_pred = bicubic(lr_image, config.upscale_factor)
+
+            loss = criterion(y_pred, y_true)
+            test_loss += loss.item()
+            test_metrics += compute_metrics(config, y_pred.detach(), y_true.detach())
+            
+        test_loss = test_loss / len(test_generator)
+        test_metrics = test_metrics / len(test_generator)
+        print('test loss:', test_loss)
+
+        if not(os.path.exists(logging_path)):
+            os.mkdir(logging_path)
+
+        metrics_name = [config.model.loss] + metrics_name
+        metrics_value = [test_loss] + list(test_metrics)
+
+        test_logger(logging_path, metrics_name, metrics_value, config.upscale_factor)
 
